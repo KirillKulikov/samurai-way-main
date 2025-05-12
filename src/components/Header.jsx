8e14eba4e9831bb432dd5bb447d9c46d9eb8d1cb
kulikov <kulikov.webfree@gmail.com>
2025-05-12 22:12:09 +0300

    add components Header, Navbar, Profile

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://www.clipartmax.com/png/full/200-2009759_panthers-logo.png" alt="" />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://wallpapers.com/images/hd/seychelles-beach-3600-x-1479-wallpaper-oxury1awhz1l00bx.jpg" alt="" />
+            </div>
+            <div>
+                <img src="https://avatars.mds.yandex.net/i?id=f9bb733fb8873fec7e64f98b67263e034b28bd54-5253517-images-thumbs&n=13" alt="" />
+                description
+            </div>
+            <div>
+                My post
+                <div>
+                    New Post
+                </div>
+                <div>
+                    <div>Post 1</div>
+                    <div>Post 2</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
